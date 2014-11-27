2a6206041909a41a023e4c5c0704718bb44ac595
DonKino <jules.buret@gmail.com>
2014-11-27 10:43:11 +0100

    script à la bonne place

--- app/js/extend_search.js
+++ app/js/extend_search.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function($) {
 	$.fn.exp = function(options) {
 		var set = $.extend({
@@ -5,28 +7,32 @@
 			duration: 200
 			}, options );
 
-		var width = this.outer();
+		 var width = this.width;
 
-		this.on("focus", function(){
-				$(this).animate({
-					width: set.width
-							}, set.duration, function(){
-					});
-			});
+		this.on('focus', function(){
+			$(this).animate({
+				width: set.width
+			}, set.duration, function(){});
+		});
 
-		this.on("blur", function(){
-				$(this).animate({
-					width: width
-							}, set.duration, function(){
-					});
-			});
+		this.on('focusout', function(){
+			$(this).animate({
+				width : 80
+			}, set.duration, function(){});
+		});
 
-		if (set.action && typeof(set.action) == "function"){
+		this.on('blur', function(){
+			$(this).animate({
+				width: width
+			}, set.duration, function(){});
+		});
+
+		if (set.action && typeof(set.action) == 'function'){
 			this.on('keypress', function(e) {
-					if (e.which === 13) {
-						set.action($(this).val());
-					}
-				});
+				if (e.which === 13) {
+					set.action($(this).val());
+				}
+			});
 		}
 		return this;
 	};
